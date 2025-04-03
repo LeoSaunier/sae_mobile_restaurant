e90aef3c53cc20ff0ad0fdcb571f58139f2b5cd3
Leo-sau <leo.saunier@etu.univ-orleans.fr>
2025-04-03 21:16:04 +0200

    Restaurant favoris + cuisine favoris

--- lib/database/database_service.dart
+++ lib/database/database_service.dart
@@ -13,13 +13,14 @@ class DatabaseService {
       final data = await _supabase
           .from('Restaurants')
           .select()
-      .limit(limit);
+          .limit(limit);
 
       print(data);
 
       List<Restaurant> restaurants = [];
       for (var restaurantData in data as List) {
-        final cuisines = await getCuisinesForRestaurant(restaurantData['restaurant_id']);
+        final cuisines = await getCuisinesForRestaurant(
+            restaurantData['restaurant_id']);
         restaurants.add(Restaurant.fromJson(restaurantData, cuisines));
       }
       return restaurants;
@@ -35,7 +36,9 @@ class DatabaseService {
           .select('Cuisines(name)')
           .eq('restaurant_id', restaurantId);
 
-      return (data as List).map((c) => c['Cuisines']['name'] as String).toList();
+      return (data as List)
+          .map((c) => c['Cuisines']['name'] as String)
+          .toList();
     } catch (e) {
       throw Exception('Failed to fetch cuisines for restaurant: $e');
     }
@@ -67,13 +70,16 @@ class DatabaseService {
       }
 
       // Récupérer les noms des cuisines depuis la réponse
-      final cuisinesResponse = response['Restaurants_Cuisines'] as List<dynamic>;
+      final cuisinesResponse = response['Restaurants_Cuisines'] as List<
+          dynamic>;
       final cuisinesNames = cuisinesResponse
           .map((c) => c['cuisine_id'])
           .toList();
 
       final cuisineNamesResponse = response['Cuisines'] as List<dynamic>;
-      final cuisines = cuisineNamesResponse.map((c) => c['name'] as String).toList();
+      final cuisines = cuisineNamesResponse
+          .map((c) => c['name'] as String)
+          .toList();
 
       // Retourner un restaurant avec les cuisines récupérées
       return Restaurant.fromJson(response, cuisines);
@@ -82,7 +88,6 @@ class DatabaseService {
       return null;
     }
   }
-
 
   Future<List<Restaurant>> getRestaurantsByCuisine(String cuisine) async {
     try {
@@ -101,7 +106,9 @@ class DatabaseService {
       // Parcourir la réponse et extraire les informations des restaurants et des cuisines
       for (var restaurantData in response) {
         final cuisinesResponse = restaurantData['Cuisines'] as List<dynamic>;
-        final cuisines = cuisinesResponse.map((c) => c['name'] as String).toList();
+        final cuisines = cuisinesResponse
+            .map((c) => c['name'] as String)
+            .toList();
 
         // Créer un objet Restaurant avec les données récupérées
         final restaurant = Restaurant.fromJson(restaurantData, cuisines);
@@ -149,25 +156,27 @@ class DatabaseService {
           .select('name')
           .eq('id', response['favorite_cuisines']);
 
-      final cuisines = cuisinesResponse.map<String>((cuisine) => cuisine['name']).toList();
+      final cuisines = cuisinesResponse.map<String>((
+          cuisine) => cuisine['name']).toList();
 
       final restaurantsResponse = await _supabase
           .from('Restaurants')
           .select('name')
           .eq('id', response['favorite_restaurants']);
 
-      final restaurants = restaurantsResponse.map<String>((restaurant) => restaurant['name']).toList();
+      final restaurants = restaurantsResponse.map<String>((
+          restaurant) => restaurant['name']).toList();
 
       // Créer l'objet User avec les cuisines et restaurants favoris
       return Utilisateur.fromJson(response, cuisines, restaurants);
-
     } catch (error) {
       print('Erreur lors de la récupération de l\'utilisateur: $error');
       return null;
     }
   }
 
-  Future<Utilisateur?> registerUser(String prenom, String nom, String email, String password) async {
+  Future<Utilisateur?> registerUser(String prenom, String nom, String email,
+      String password) async {
     try {
       // 1. Hachage du mot de passe (vous pouvez aussi utiliser une méthode de hachage côté serveur si nécessaire)
       final hashedPassword = await hashPassword(password);
@@ -183,7 +192,8 @@ class DatabaseService {
       ]);
 
       if (response.error != null) {
-        print('Erreur lors de l\'insertion de l\'utilisateur: ${response.error?.message}');
+        print('Erreur lors de l\'insertion de l\'utilisateur: ${response.error
+            ?.message}');
         return null;
       }
 
@@ -193,14 +203,15 @@ class DatabaseService {
       print('Erreur lors de l\'enregistrement de l\'utilisateur: $error');
       return null;
     }
-    }
+  }
 
-    Future<String> hashPassword(String password) async {
-      // bcrypt pour générer le hash du mot de passe
-      final salt = BCrypt.gensalt(); // Générer un salt
-      final hashedPassword = BCrypt.hashpw(password, salt);  // Hacher le mot de passe avec le salt
-      return hashedPassword;
-    }
+  Future<String> hashPassword(String password) async {
+    // bcrypt pour générer le hash du mot de passe
+    final salt = BCrypt.gensalt(); // Générer un salt
+    final hashedPassword = BCrypt.hashpw(
+        password, salt); // Hacher le mot de passe avec le salt
+    return hashedPassword;
+  }
 
   Future<double?> getRestaurantRating(int restaurantId) async {
     final response = await _supabase
@@ -234,7 +245,8 @@ class DatabaseService {
       id: response['id'],
       firstName: response['prenom'],
       email: response['email'],
-      passwordHash: '', // Optionnel : Ne pas stocker le hash dans l'objet
+      passwordHash: '',
+      // Optionnel : Ne pas stocker le hash dans l'objet
       lastName: response['nom'],
       favoriteCuisines: [],
       favoriteRestaurants: [],
@@ -270,7 +282,6 @@ class DatabaseService {
     return reviewsList;
   }
 
-
   // Fonction pour récupérer tous les avis d'un utilisateur en se basant uniquement sur les ids
   Future<List<Review>> getReviewsUser(int userId) async {
     final response = await _supabase
@@ -285,19 +296,20 @@ class DatabaseService {
     for (var reviewJson in response) {
       // Créer l'objet Review et l'ajouter à la liste
       reviewsList.add(Review(
-      id: reviewJson['reviews_id'],
-      Idrestaurant: reviewJson['restaurant_id'],
-      Iduser: userId,
-      rating: reviewJson['rating'],
-      comment: reviewJson['comment'],
-      date: DateTime.parse(reviewJson['created_at']),
+        id: reviewJson['reviews_id'],
+        Idrestaurant: reviewJson['restaurant_id'],
+        Iduser: userId,
+        rating: reviewJson['rating'],
+        comment: reviewJson['comment'],
+        date: DateTime.parse(reviewJson['created_at']),
       ));
     }
 
     return reviewsList;
-    }
+  }
 
-  Future<void> addReview(int restaurantId, int userId, double rating, String comment) async {
+  Future<void> addReview(int restaurantId, int userId, double rating,
+      String comment) async {
     final response = await _supabase
         .from('Reviews')
         .insert([
@@ -306,7 +318,8 @@ class DatabaseService {
         'user_id': userId,
         'rating': rating,
         'comment': comment,
-        'created_at': DateTime.now().toIso8601String()  // Optionnel si tu veux définir la date
+        'created_at': DateTime.now().toIso8601String()
+        // Optionnel si tu veux définir la date
       }
     ]);
 
@@ -331,12 +344,188 @@ class DatabaseService {
       }
     } catch (e) {
       // Gérer l'exception
-      throw Exception('Erreur lors de la récupération des cuisines favorites : ${e}');
+      throw Exception(
+          'Erreur lors de la récupération des cuisines favorites : ${e}');
       // Vous pouvez également afficher un message à l'utilisateur ou effectuer d'autres actions
     }
 
     return favoriteCuisines;
   }
+
+  Future<void> addCuisineFavorite(int userId, String cuisineName) async {
+    try {
+      // 1. Récupérer l'ID de la cuisine à partir de son nom
+      final cuisineResponse = await _supabase
+          .from('Cuisines')
+          .select('cuisine_id')
+          .eq('name', cuisineName)
+          .maybeSingle();
+
+      if (cuisineResponse == null) {
+        print('Cuisine non trouvée');
+        return;
+      }
+
+      final cuisineId = cuisineResponse['cuisine_id'];
+
+      // 2. Ajouter la cuisine aux favoris de l'utilisateur
+      final response = await _supabase.from('Cuisines_Favoris').insert([
+        {
+          'user_id': userId,
+          'cuisine_id': cuisineId,
+        }
+      ]);
+
+      if (response.error != null) {
+        print('Erreur lors de l\'ajout de la cuisine aux favoris : ${response
+            .error?.message}');
+      } else {
+        print('Cuisine ajoutée avec succès aux favoris');
+      }
+    } catch (error) {
+      print('Erreur lors de l\'ajout de la cuisine aux favoris: $error');
+    }
+  }
+
+  Future<void> deleteCuisineFavorite(int userId, String cuisineName) async {
+    try {
+      // 1. Récupérer l'ID de la cuisine en fonction du nom
+      final cuisineResponse = await _supabase
+          .from('Cuisines')
+          .select('cuisine_id')
+          .eq('name', cuisineName)
+          .maybeSingle();
+
+      if (cuisineResponse == null) {
+        print('Cuisine non trouvée');
+        return;
+      }
+
+      final cuisineId = cuisineResponse['cuisine_id'];
+
+      // 2. Supprimer l'entrée correspondante dans Cuisines_Favoris
+      final deleteResponse = await _supabase
+          .from('Cuisines_Favoris')
+          .delete()
+          .eq('user_id', userId)
+          .eq('cuisine_id', cuisineId);
+
+      if (deleteResponse.error != null) {
+        print('Erreur lors de la suppression : ${deleteResponse.error?.message}');
+      } else {
+        print('Cuisine favorite supprimée avec succès');
+      }
+    } catch (error) {
+      print('Erreur lors de la suppression de la cuisine favorite : $error');
+    }
+  }
+
+  Future<List<Restaurant>> getRestaurantsFavorite(int userId) async {
+    try {
+      // 1. Récupérer les IDs des restaurants favoris de l'utilisateur
+      final favoritesResponse = await _supabase
+          .from('Restaurant_Favoris')
+          .select('restaurant_id')
+          .eq('user_id', userId);
+
+      if (favoritesResponse.isEmpty) {
+        return []; // Aucun restaurant favori trouvé
+      }
+
+      // 2. Récupérer les détails des restaurants favoris
+      final restaurantIds = favoritesResponse.map<int>((
+          fav) => fav['restaurant_id'] as int).toList();
+      final restaurantsResponse = await _supabase
+          .from('Restaurants')
+          .select('*')
+          .inFilter('restaurant_id', restaurantIds);
+
+      if (restaurantsResponse.isEmpty) {
+        return []; // Aucun détail de restaurant trouvé
+      }
+
+      // 3. Récupérer les cuisines associées à chaque restaurant
+      List<Restaurant> favoriteRestaurants = [];
+      for (var restaurantData in restaurantsResponse) {
+        final restaurantId = restaurantData['restaurant_id'] as int;
+
+        // Récupérer les cuisines associées au restaurant
+        final cuisinesResponse = await _supabase
+            .from('Restaurants_Cuisines')
+            .select('cuisine_id')
+            .eq('restaurant_id', restaurantId);
+
+        List<String> cuisines = [];
+        for (var cuisineData in cuisinesResponse) {
+          final cuisineId = cuisineData['cuisine_id'] as int;
+          final cuisineDetailResponse = await _supabase
+              .from('Cuisines')
+              .select('name')
+              .eq('cuisine_id', cuisineId)
+              .maybeSingle();
+
+          if (cuisineDetailResponse != null) {
+            cuisines.add(cuisineDetailResponse['name'] as String);
+          }
+        }
+
+        // Créer un objet Restaurant avec les données récupérées
+        Restaurant restaurant = Restaurant(
+          restaurantId: restaurantData['restaurant_id'] as int,
+          name: restaurantData['name'] as String,
+          address: restaurantData['address'] as String,
+          cuisines: cuisines,
+          // Ajouter d'autres champs selon la structure de votre table Restaurants
+        );
+
+        favoriteRestaurants.add(restaurant);
+      }
+
+      return favoriteRestaurants;
+    } catch (error) {
+      print('Erreur lors de la récupération des restaurants favoris : $error');
+      return [];
+    }
+  }
+
+  Future<void> addRestaurantFavorite(int userId, int restaurantId) async {
+    try {
+      final response = await _supabase.from('Restaurant_Favoris').upsert([
+        {
+          'user_id': userId,
+          'restaurant_id': restaurantId,
+        }
+      ]);
+
+      if (response.error != null) {
+        print('Erreur lors de l\'ajout aux favoris : ${response.error?.message}');
+      } else {
+        print('Restaurant ajouté aux favoris avec succès');
+      }
+    } catch (error) {
+      print('Erreur lors de l\'ajout du restaurant aux favoris : $error');
+    }
+  }
+
+  Future<void> removeRestaurantFavorite(int userId, int restaurantId) async {
+    try {
+      final response = await _supabase
+          .from('Restaurant_Favoris')
+          .delete()
+          .eq('user_id', userId)
+          .eq('restaurant_id', restaurantId);
+
+      if (response.error != null) {
+        print('Erreur lors de la suppression des favoris : ${response.error?.message}');
+      } else {
+        print('Restaurant supprimé des favoris avec succès');
+      }
+    } catch (error) {
+      print('Erreur lors de la suppression du restaurant des favoris : $error');
+    }
+  }
+
+
 
 }
 
